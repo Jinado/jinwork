05099091628666232faf3ed6768d36b39e1d79b6
Johannes Emmoth <johannes.emmoth@gmail.com>
2022-02-09 19:17:23 +0100

    Added status code and begun fleshing out the Response class

--- backend/src/Routing/Response/Response.php
+++ backend/src/Routing/Response/Response.php
@@ -4,11 +4,36 @@ namespace Jinwork\Routing;
 
 use JetBrains\PhpStorm\NoReturn;
 
+require_once __DIR__ . '/StatusCode.php';
+
 /**
  * @since 1.1.0-alpha
  */
 class Response
 {
+    /**
+     * @var string
+     */
+    protected string $content;
+
+    /**
+     * @var array
+     */
+    protected array $headers;
+
+    /**
+     * @var StatusCode|int
+     */
+    protected StatusCode|int $statusCode;
+
+    /**
+     * @since 1.1.0-alpha
+     */
+    public function __construct()
+    {
+        $this->statusCode = StatusCode::OK;
+    }
+
     #[NoReturn] public function send($content)
     {
         // TODO: Add headers
